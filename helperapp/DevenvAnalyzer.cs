6ba668e150dbecf512e2f132eaa0593acdae0978
Amadeusz Wieczorek <git@amadeusw.com>
2018-01-18 23:53:42 -0800

    gets it all done

---
+++ helperapp/DevenvAnalyzer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helperapp
+{
+    static class DevenvAnalyzer
+    {
+        static Dictionary<string, string> PathToIdMap = new Dictionary<string, string>();
+        const string installationIdString = "InstallationID";
+
+        public static string GetVsId(string path)
+        {
+            string installationId;
+            if (!PathToIdMap.TryGetValue(path, out installationId))
+            {
+                var directory = Path.GetDirectoryName(path);
+                var configurationFile = Path.Combine(directory, "devenv.isolation.ini");
+                foreach (var line in File.ReadAllLines(configurationFile))
+                {
+                    if (line.StartsWith(installationIdString))
+                    {
+                        installationId = line.Substring(line.IndexOf('=') + 1);
+                        PathToIdMap[path] = installationId;
+                        return installationId;
+                    }
+                }
+                throw new Exception($"Could not find {installationIdString} in {configurationFile}");
+            }
+            return installationId;
+        }
+
+        internal static string GetMefErrorsPath(string id, string hive)
+        {
+            return Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Microsoft\\VisualStudio\\15.0_" + id + hive,
+                "ComponentModelCache",
+                "Microsoft.VisualStudio.Default.err");
+        }
+
+        internal static (string, string) GetCommandLinePathAndArgs(string id, string path, string hive)
+        {
+            var cmdPath = Environment.ExpandEnvironmentVariables("%comspec%");
+            var args = "/k \"" + Path.Combine(
+                Path.GetDirectoryName(path),
+                "..\\Tools",
+                "VsDevCmd.bat")
+                + "\"";
+            return (cmdPath, args);
+        }
+
+        internal static string GetActivityLogPath(string id, string hive)
+        {
+            return Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Microsoft\\VisualStudio\\15.0_" + id + hive,
+                "ActivityLog.xml");
+        }
+
+        internal static string GetInstallationPath(string path)
+        {
+            return Path.GetDirectoryName(path);
+        }
+    }
+}
--- helperapp/MainWindow.xaml.cs
+++ helperapp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,7 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using PInvoke;
+using System.Management;
 
 namespace helperapp
 {
@@ -24,13 +28,128 @@ namespace helperapp
         public MainWindow()
         {
             InitializeComponent();
+            ThisWindow = this;
         }
 
-        private void Window_Drop(object sender, DragEventArgs e)
+        public delegate void WinEventProc(IntPtr hWinEventHook, User32.WindowsEventHookType @event, IntPtr hwnd, int idObject, int idChild, int dwEventThread, uint dwmsEventTime);
+        WinEventProc Listener;
+        static MainWindow ThisWindow;
+
+        public string RecentId { get; private set; }
+        public string RecentPath { get; private set; }
+        public string RecentHive { get; private set; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var point = e.GetPosition(null);
-            var win32point = new POINT { x = (int)point.X, y = (int)point.Y };
-            var hwnd = User32.WindowFromPoint(win32point);
+            try
+            {
+                Listener = new WinEventProc(target);
+                var functionPointer = Marshal.GetFunctionPointerForDelegate(Listener);
+                User32.SetWinEventHook(User32.WindowsEventHookType.EVENT_SYSTEM_FOREGROUND, User32.WindowsEventHookType.EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, functionPointer, 0, 0, User32.WindowsEventHookFlags.WINEVENT_OUTOFCONTEXT);
+            }
+            catch (Exception ex)
+            {
+                ThisWindow.Status.Text = ex.Message;
+            }
+        }
+
+        static void target(IntPtr hWinEventHook, User32.WindowsEventHookType @event, IntPtr hwnd, int idObject, int idChild, int dwEventThread, uint dwmsEventTime)
+        {
+            try
+            {
+                var threadId = User32.GetWindowThreadProcessId(hwnd, out int processId);
+                if (processId == 0)
+                {
+                    ThisWindow.Status.Text = "Unable to get process data";
+                }
+                else
+                {
+                    String[] properties = { "Name", "ExecutablePath", "CommandLine" };
+                    SelectQuery s = new SelectQuery("Win32_Process",
+                       $"ProcessID = '{processId}' ",
+                       properties);
+
+                    ManagementObjectSearcher searcher =
+                       new ManagementObjectSearcher(s);
+
+                    foreach (ManagementObject o in searcher.Get())
+                    {
+                        if (o["Name"].ToString() != "devenv.exe")
+                        {
+                            return;
+                        }
+
+                        var id = DevenvAnalyzer.GetVsId(o["ExecutablePath"].ToString());
+                        UpdateUI(id, o["CommandLine"].ToString(), o["ExecutablePath"].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ThisWindow.Status.Text = ex.Message;
+            }
+        }
+
+        private static void UpdateUI(string id, string arguments, string path)
+        {
+            var rootSuffix = arguments.Split('/').FirstOrDefault(n => n.StartsWith("rootSuffix"))?.Substring("rootSuffix".Length + 1);
+            ThisWindow.Status.Text = "Microsoft Visual Studio 2017 " + rootSuffix;
+            ThisWindow.RecentId = id;
+            ThisWindow.RecentPath = path;
+            ThisWindow.RecentHive = rootSuffix;
+            ThisWindow.AllUI.Visibility = Visibility.Visible;
+        }
+
+        private void OnMefClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var mefPath = DevenvAnalyzer.GetMefErrorsPath(RecentId, RecentHive);
+                Process.Start(mefPath);
+            }
+            catch(Exception ex)
+            {
+                Status.Text = ex.Message;
+            }
+        }
+
+        private void OnActivityLogClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var activityLogPath = DevenvAnalyzer.GetActivityLogPath(RecentId, RecentHive);
+                Process.Start(activityLogPath);
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.Message;
+            }
+        }
+
+        private void OnCommandPromptClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var (commandLinePath, args) = DevenvAnalyzer.GetCommandLinePathAndArgs(RecentId, RecentPath, RecentHive);
+                Process.Start(commandLinePath, args);
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.Message;
+            }
+        }
+
+        private void OnInstallLocationClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var installationPath = DevenvAnalyzer.GetInstallationPath(RecentPath);
+                Process.Start(installationPath);
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.Message;
+            }
         }
     }
 }
