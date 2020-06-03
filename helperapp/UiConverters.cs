e13fc2f1ba900857a523bb7d641804ea96abfde5
Amadeusz Wieczorek <amwieczo@microsoft.com>
2020-06-02 23:44:46 -0700

    ui

--- helperapp/UiConverters.cs
+++ helperapp/UiConverters.cs
@@ -32,4 +32,18 @@ namespace helperapp
             throw new NotImplementedException();
         }
     }
+
+    [ValueConversion(sourceType: typeof(string), targetType: typeof(Visibility))]
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
