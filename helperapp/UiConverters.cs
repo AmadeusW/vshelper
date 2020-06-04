2a137d694804e2cf41d999e308d669005e7e2cb4
Amadeusz Wieczorek <amwieczo@microsoft.com>
2020-06-04 00:03:01 -0700

    show buttons only when we have data

--- helperapp/UiConverters.cs
+++ helperapp/UiConverters.cs
@@ -46,4 +46,18 @@ namespace helperapp
             throw new NotImplementedException();
         }
     }
+
+    [ValueConversion(sourceType: typeof(object), targetType: typeof(Visibility))]
+    public class NotNullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
